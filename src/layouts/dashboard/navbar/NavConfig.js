f530d04030d765a8464f2fa7ce20aced3ef30a9e
park <park@parkui-MacBookPro.local>
2023-09-15 13:25:06 +0900

    v1.8.0

--- src/layouts/dashboard/navbar/NavConfig.js
+++ src/layouts/dashboard/navbar/NavConfig.js
@@ -16,7 +16,7 @@ const sidebarConfig = [
   // GENERAL
   // ----------------------------------------------------------------------
   {
-    subheader: 'snpsystem v1.7.7',
+    subheader: 'snpsystem v1.8.0',
     items: [
       // { title: '맵', path: '/map', icon: ICONS.ecommerce },
       { title: '에스엔피 오픈마켓집계', path: '/dashboard/snp', icon: ICONS.ecommerce },
